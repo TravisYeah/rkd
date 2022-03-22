e5e171084457b7d8921b20b08c98aaab0cd001e3
Travis Harrison <travisharrisonyeah@gmail.com>
2022-03-22 15:03:23 -0500

    add compression benchmark for multiple operations

--- compression/src/main.rs
+++ compression/src/main.rs
@@ -7,7 +7,36 @@ fn fixed_width_write<'a>(writer: &mut Writer<std::fs::File>, record: &Vec<&'a st
     .unwrap();
 }
 
-fn delta_compress(source_path: &str, target_path: &str) {
+fn delta_compress(writer: &mut Writer<std::fs::File>, title: &str, description: &str, source_path: &str, target_path: &str) {
+  let mut source_file = std::fs::File::open(source_path).unwrap();
+  let mut target_file = std::fs::File::open(target_path).unwrap();
+  let mut source = Vec::new();
+  let mut target = Vec::new();
+  source_file.read_to_end(&mut source).unwrap();
+  target_file.read_to_end(&mut target).unwrap();
+  let q = 10_usize.pow(9) + 9;
+  let rk = rkpb::RabinKarp::new(q);
+  let mut copies: Vec<rkpb::Match> = Vec::new();
+  let window = 1 << 7;
+  rk.search(&source, &target, window, &mut copies);
+  let mut delta = Vec::new();
+  rk.compress(&target, &copies, &mut delta);
+  let source_parts = source_path.split("/").collect::<Vec<&str>>();
+  let target_parts = target_path.split("/").collect::<Vec<&str>>();
+  fixed_width_write(
+    writer,
+    &Vec::from([
+      title, description,
+      source_parts[source_parts.len() - 1],
+      target_parts[target_parts.len() - 1],
+      source.len().to_string().as_str(),
+      target.len().to_string().as_str(),
+      delta.len().to_string().as_str(),
+    ]),
+  );
+}
+
+fn main() {
   let mut writer = WriterBuilder::new()
     .delimiter(b'\t')
     .from_path("compression.csv")
@@ -24,36 +53,18 @@ fn delta_compress(source_path: &str, target_path: &str) {
       "delta_size",
     ]),
   );
-  let mut source_file = std::fs::File::open(source_path).unwrap();
-  let mut target_file = std::fs::File::open(target_path).unwrap();
-  let mut source = Vec::new();
-  let mut target = Vec::new();
-  source_file.read_to_end(&mut source).unwrap();
-  target_file.read_to_end(&mut target).unwrap();
-  let q = 10_usize.pow(9) + 9;
-  let rk = rkpb::RabinKarp::new(q);
-  let mut copies: Vec<rkpb::Match> = Vec::new();
-  let window = 1 << 7;
-  rk.search(&source, &target, window, &mut copies);
-  let mut delta = Vec::new();
-  rk.compress(&target, &copies, &mut delta);
-  fixed_width_write(
-    &mut writer,
-    &Vec::from([
-      "rabin karp wiki",
-      "insert middle",
-      "rk-wiki.txt",
-      "rk-wiki-insert-p.txt",
-      source.len().to_string().as_str(),
-      target.len().to_string().as_str(),
-      delta.len().to_string().as_str(),
-    ]),
-  );
-}
-
-fn main() {
   delta_compress(
+    &mut writer,
+    "rabin karp wiki",
+    "insert middle",
     "../benches/data/rk-wiki.txt",
     "../benches/data/rk-wiki-insert-p.txt",
+  );
+  delta_compress(
+    &mut writer,
+    "rabin karp wiki",
+    "multiple ops",
+    "../benches/data/rk-wiki.txt",
+    "../benches/data/rk-wiki-multi-ops.txt",
   );
 }
